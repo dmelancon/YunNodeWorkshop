db51ab7b9b33a3215a3dd41b5917684a256f7269
dmelancon <dmelancon.d@gmail.com>
2015-01-19 18:48:19 -0500

    updated Files

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var WebSocket = require('ws')
+  , ws = new WebSocket('ws://192.168.1.13:3000/');
+
+ws.on('error', function(){
+  console.log("Yun WS Server not found");
+});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/test.html');
@@ -8,10 +14,14 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('message', function(msg){
+    msg = msg.replace(/(<([^>]+)>)/ig,"");
     io.emit('message', msg);
+    msg = msg.replace(/&nbsp;/g,'');
+    ws.send(msg+"\n",function ack(error){});
+    console.log(msg);
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(8000, function(){
+  console.log('listening on *:8000');
 });
